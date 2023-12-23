58a10ab80a93842c3491b2716c109aaa6b4f7f7c
warrior-robotics <warriorrobotics@outlook.com>
2023-12-23 14:17:10 -0600

    SwerveKinematics set up

--- src/main/java/frc/robot/SwerveDrive.java
+++ src/main/java/frc/robot/SwerveDrive.java
@@ -1,46 +1,55 @@
 package frc.robot;
 
-public class SwerveDrive {
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
-    public final double L = 0.5588;
-    public final double W = 0.5588;
+public class SwerveDrive {
+    
+    // Distance from center for wheels. Currently assumes rectangulare wheel arrangement
+    private double frontWheelPosition = 0.2604;
+    private double backWheelPosition = -0.2604;
+    private double rightWheelPosition = -0.2786;
+    private double leftWheelPosition = 0.2786;
 
     private WheelDrive backRight;
     private WheelDrive backLeft;
     private WheelDrive frontRight;
     private WheelDrive frontLeft;
 
+    // Kinematics
+    private SwerveDriveKinematics swerveKinematics;
+    private ChassisSpeeds chassisSpeeds;
+    
+
     public SwerveDrive(WheelDrive backRight, WheelDrive backLeft, WheelDrive frontRight, WheelDrive frontLeft) {
         this.backRight = backRight;
         this.backLeft = backLeft;
         this.frontRight = frontRight;
         this.frontLeft = frontLeft;
+
+        Translation2d frontRightLocation = new Translation2d(frontWheelPosition, rightWheelPosition);
+        Translation2d frontLeftLocation = new Translation2d(frontWheelPosition, leftWheelPosition);
+        Translation2d backLeftLocation = new Translation2d(backWheelPosition, leftWheelPosition);
+        Translation2d backRightLocation = new Translation2d(backWheelPosition, rightWheelPosition);
+
+        swerveKinematics = new SwerveDriveKinematics(frontRightLocation, frontLeftLocation, backLeftLocation, backRightLocation);
     }
 
-    public void drive (double x1, double y1, double x2){
-        double r = Math.sqrt ((L * L) + (W * W));
-        y1 *= -1;
-        x1 *= -1;
-        x2 *= -1;
+    public void driveWithKinematics(double leftStickX, double leftStickY, double rightStickX) {
+        // The commented out values are copied from last year's
+        // double xAxisSpeed = Math.copySign(Math.pow(leftStickX, 2.0), leftStickX);
+        // double yAxisSpeed = -Math.copySign(Math.pow(leftStickY, 2.0), leftStickY);
+        // double spinSpeed = Math.copySign(Math.pow(rightStickX, 2.0), rightStickX);
 
-        double a = x1 - x2 * (L / r);
-        double b = x1 + x2 * (L / r);
-        double c = y1 - x2 * (W / r);
-        double d = y1 + x2 * (W / r);
+        chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(-leftStickY, leftStickX, rightStickX, new Rotation2d(0));
 
-        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
-        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
-        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
-        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
-
-        double backRightAngle = (Math.atan2 (a, d) / Math.PI) * 180;
-        double backLeftAngle = (Math.atan2 (a, c) / Math.PI) * 180;
-        double frontRightAngle = (Math.atan2 (b, d) / Math.PI) * 180;
-        double frontLeftAngle = (Math.atan2 (b, c) / Math.PI) * 180;
-
-        backRight.drive(backRightSpeed, backRightAngle);
-        backLeft.drive(backLeftSpeed, backLeftAngle);
-        frontRight.drive(frontRightSpeed, frontRightAngle);
-        frontLeft.drive(frontLeftSpeed, frontLeftAngle);
+        SwerveModuleState[] moduleStates = swerveKinematics.toSwerveModuleStates(chassisSpeeds);
+        frontLeft.driveKinematics(moduleStates[0]);
+        frontRight.driveKinematics(moduleStates[1]);
+        backRight.driveKinematics(moduleStates[2]);
+        backLeft.driveKinematics(moduleStates[3]);
     }
 }
